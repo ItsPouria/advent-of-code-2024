84df10374f1371ff8a3a634b0ff4064347e251a0
ItsPouria <pouriamemarvar@gmail.com>
2024-12-01 19:35:15 +0100

    initialize rust project with day one required files

---
+++ day_1/target/debug/deps/day_1-ddf82e8a855406e0.d
@@ -0,0 +1,5 @@
+/Users/pouriamemarvar/Desktop/My Projects/advent-of-code-2024/day_1/target/debug/deps/day_1-ddf82e8a855406e0: src/main.rs
+
+/Users/pouriamemarvar/Desktop/My Projects/advent-of-code-2024/day_1/target/debug/deps/day_1-ddf82e8a855406e0.d: src/main.rs
+
+src/main.rs:
